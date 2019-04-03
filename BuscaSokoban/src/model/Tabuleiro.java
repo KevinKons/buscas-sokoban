f891815add4f800eb2efafec0ad3bf4b5719a659
Douglas <martins.douglas.souza@gmail.com>
2019-04-03 20:28:29 -0300

    Esqueci de adicionar os arquivos no ultimo commit

--- BuscaSokoban/src/model/Tabuleiro.java
+++ BuscaSokoban/src/model/Tabuleiro.java
@@ -4,8 +4,8 @@ public class Tabuleiro {
 
     private int[][] tabuleiro;
 
-    public Tabuleiro(int x, int y){
-        tabuleiro = new int[x][y];
+    public Tabuleiro(int[][] tabuleiro){
+        this.tabuleiro = tabuleiro;
     }
 
 }
--- BuscaSokoban/src/util/LeitorArquivo.java
+++ BuscaSokoban/src/util/LeitorArquivo.java
@@ -1,4 +1,41 @@
 package util;
 
+import model.Tabuleiro;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Logger;
+
 public class LeitorArquivo {
+
+    // Um método de leitura que retorna um Tabuleiro
+    // nomeArquivo.txt
+    public Tabuleiro ler(String nomeArquivo) throws Exception {
+
+        int[][] tabuleiro;
+        boolean primeiraLinha = true;
+
+        try {
+            BufferedReader ler = new BufferedReader(new FileReader(nomeArquivo));
+            String str;
+            while ((str = ler.readLine()) != null) {
+
+                if (primeiraLinha) {
+                    int x = (int) str.charAt(0);
+                    int y = (int) str.charAt(2);
+                    tabuleiro = new int[x][y];
+                    primeiraLinha = false;
+                }
+
+
+
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ex.getMessage());
+        }
+
+        return null;
+    }
+
 }
