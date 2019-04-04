2d6de8e061a510b9b2c361d6d638e94937843b01
Douglas <martins.douglas.souza@gmail.com>
2019-04-04 16:45:44 -0300

    Leitor de arquivos modificados
    O leitor de arquivos está gravando na matriz cada char do arquivo texto e está retornando o número de caixas no mapa

--- BuscaSokoban/src/util/LeitorArquivo.java
+++ BuscaSokoban/src/util/LeitorArquivo.java
@@ -5,36 +5,54 @@ import model.Tabuleiro;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class LeitorArquivo {
 
-    public Tabuleiro ler(String nomeArquivo) throws Exception {
+    private static Tabuleiro ler(String nomeArquivo) throws Exception {
 
-        char[][] tabuleiro;
+        char[][] tabuleiro = new char[0][];
         int qtdCaixas = 0;
+        int linha = 0;
         boolean primeiraLinha = true;
 
-        try {
-            BufferedReader ler = new BufferedReader(new FileReader(nomeArquivo));
-            String str;
-            while ((str = ler.readLine()) != null) {
 
+        try {
+            BufferedReader ler = new BufferedReader(new FileReader("../" + nomeArquivo));
+            String str;
+
+            while ((str = ler.readLine()) != null) {
+                // Pegando as informações da primeira linha
                 if (primeiraLinha) {
                     int x = (int) str.charAt(0);
                     int y = (int) str.charAt(2);
                     tabuleiro = new char[x][y];
                     primeiraLinha = false;
                 }
+                // Gravando as informações do arquivo texto na matriz de Char
+                for (int i = 0; i < str.length() - 1; i++) {
+                    tabuleiro[linha][i] = str.charAt(i);
+                    if (String.valueOf(str.charAt(i)).equals("$")) {
+                        qtdCaixas++;
+                    }
+                }
+//                System.out.println(str);
 
-                
+                linha++;
 
             }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
-
-        return null;
+//        System.out.println("Quantidade caixas:" + qtdCaixas);
+        return new Tabuleiro(tabuleiro, 0);
     }
+
+//    public static void main(String[] args) throws Exception {
+//
+//        ler("alberto(1).txt");
+//
+//    }
 
 }
