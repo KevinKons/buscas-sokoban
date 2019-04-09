d9ff70edc014137626f3d0d7a7e5fae91f8e4caf
KevinKons <kevin.kons@hotmail.com>
2019-04-08 21:39:02 -0300

    classe main

--- BuscaSokoban/src/Main.java
+++ BuscaSokoban/src/Main.java
@@ -1,6 +1,28 @@
+import jomi.BuscaLargura;
+import jomi.Nodo;
+import model.EstadoSokoban;
+import model.Tabuleiro;
+import util.LeitorArquivo;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o nome da instância que o agente deve buscar a solução");
+        String instancia = sc.nextLine();
+        Tabuleiro tabuleiro = LeitorArquivo.ler(instancia);
+        System.out.println(tabuleiro.toString());
+
+        Nodo n = new BuscaLargura().busca(new EstadoSokoban(tabuleiro));
+        if (n == null) {
+            System.out.println("sem solucao!");
+        } else {
+            System.out.println("solucao:\n" + n.montaCaminho() + "\n\n");
+        }
+
+
     }
 }
